ef43a076bbac7dd39d2941b246cc68cdb0621f42
fodare <folorunsodamilare007@hotmail.com>
2024-03-12 19:53:52 +0100

    Update virtual machine creation template.

--- Simple_Linux_VM/main.tf
+++ Simple_Linux_VM/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  skip_provider_registration = true
   features {}
 }
 
@@ -18,56 +18,56 @@ resource "azurerm_resource_group" "rg" {
   location = var.desired_resource_location
 }
 
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
+resource "azurerm_virtual_network" "virtual_network" {
+  name                = "${var.virtual_network_name}-vn"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
+resource "azurerm_subnet" "virtual_subnet" {
+  name                 = "${var.virtual_subenet_name}-sn"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.virtual_network.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_public_ip" "pip" {
-  name                = "${var.prefix}-pip"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+resource "azurerm_public_ip" "public_ip" {
+  name                = "${var.public_ip_name}-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   allocation_method   = "Dynamic"
 }
 
 resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic1"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  name                = "${var.desired_network_interface_name}-nic1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
 
   ip_configuration {
     name                          = "primary"
-    subnet_id                     = azurerm_subnet.internal.id
+    subnet_id                     = azurerm_subnet.virtual_subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip.id
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
 resource "azurerm_network_interface" "internal" {
-  name                = "${var.prefix}-nic2"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  name                = "${var.desired_network_interface_name}-nic2"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.internal.id
+    subnet_id                     = azurerm_subnet.virtual_subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_network_security_group" "webserver" {
-  name                = "tls_webserver"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  name                = "${var.desired_network_security_group_name}-sg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   security_rule {
     access                     = "Allow"
     direction                  = "Inbound"
@@ -79,6 +79,17 @@ resource "azurerm_network_security_group" "webserver" {
     destination_port_range     = "443"
     destination_address_prefix = azurerm_network_interface.main.private_ip_address
   }
+  security_rule {
+    access                     = "Allow"
+    direction                  = "Inbound"
+    name                       = "ssh"
+    priority                   = 101
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    source_address_prefix      = "*"
+    destination_port_range     = "22"
+    destination_address_prefix = azurerm_network_interface.main.private_ip_address
+  }
 }
 
 resource "azurerm_network_interface_security_group_association" "main" {
@@ -86,14 +97,17 @@ resource "azurerm_network_interface_security_group_association" "main" {
   network_security_group_id = azurerm_network_security_group.webserver.id
 }
 
-resource "azurerm_linux_virtual_machine" "main" {
-  name                            = "${var.prefix}-vm"
-  resource_group_name             = azurerm_resource_group.main.name
-  location                        = azurerm_resource_group.main.location
-  size                            = "Standard_F2"
-  admin_username                  = "adminuser"
-  admin_password                  = "P@ssw0rd1234!"
-  disable_password_authentication = false
+resource "azurerm_linux_virtual_machine" "virtual_machine" {
+  name                = "${var.virtual_machine_name}-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = var.virtual_machine_size
+  admin_username      = var.virtual_machine_userName
+  admin_ssh_key {
+    username   = var.virtual_machine_userName
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+  disable_password_authentication = true
   network_interface_ids = [
     azurerm_network_interface.main.id,
     azurerm_network_interface.internal.id,
