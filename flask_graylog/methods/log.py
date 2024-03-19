31681251a0b7572e32f7cc40a6df10571c478319
fodare <folorunsodamilare007@hotmail.com>
2024-03-19 18:27:54 +0100

    Print response code if error occurs when logging to Graylog server.

--- flask_graylog/methods/log.py
+++ flask_graylog/methods/log.py
@@ -17,3 +17,5 @@ def write_log(message):
     })
     is_written = requests.post(
         f'{LOCAL_GRAYLOG_BASE_URL}:{GRAYLOG_GELF_TCP_PORT}/gelf', data=json_content, headers=headers, verify=False)
+    if is_written.status_code != 202:
+        print(f"Error writting to graylog server. Received {is_written.status_code}.")
