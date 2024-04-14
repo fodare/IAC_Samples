90e0e17434fa42db04141f5520da7880f9982f68
fodare <folorunsodamilare007@hotmail.com>
2024-04-14 13:58:11 +0200

    Update staroage account name.

--- azure_func_devops/IAC/var.tf
+++ azure_func_devops/IAC/var.tf
@@ -13,7 +13,7 @@ variable "resource_groupLocation" {
 variable "storage_account_name" {
   type        = string
   description = "Desired storage account name"
-  default     = "testsa"
+  default     = "test-sa"
 }
 
 variable "service_plan_name" {
