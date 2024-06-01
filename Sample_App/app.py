55f770dfb4b6dbc040a63d987dfb22d1588a753e
fodare <folorunsodamilare007@hotmail.com>
2024-06-02 00:13:08 +0200

    Add service health and template resource.

--- Sample_App/app.py
+++ Sample_App/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, make_response
+import platform
+import socket
+import datetime
 import requests
 from helpers import get_rate
 import os
 
 app = Flask(__name__)
 app_listening_port = int(os.environ.get('PORT', 5000))
+
+app_version = os.environ.get('app_version', "V.0.1")
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -18,6 +23,17 @@ def get_wise_comparison():
         wisePartnerRates = get_rate(
             sourceCurrency, destinationCurrency, amount)
         return render_template('index.html', partnerRates=wisePartnerRates)
+
+
+@app.route("/resource", methods=['GET'])
+def get_app_resource_info():
+    system_status = {
+        "serverName": socket.gethostname(),
+        "platform": platform.system(),
+        "serverTime": datetime.datetime.now(),
+        "appVersion": app_version
+    }
+    return render_template('resourceInfo.html', systemInfo=system_status)
 
 
 if __name__ == "__main__":
